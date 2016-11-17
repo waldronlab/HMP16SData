ac7501e7323a177357b144a86924afd9cf055758
Rimsha Azhar <rimsha@Rimshas-MacBook-Air.local>
2016-11-17 16:25:12 -0500

    first command

--- R/Untitled.R
+++ R/Untitled.R
@@ -2,7 +2,7 @@
 data <- read_tsv("./myfile.tsv")
 
 # add the rownames from last column
-data[, 231] <- gsub("Root;", "", data[, 231])
+data[, 233] <- gsub("Root;", "", data[, 233])
 data[, 231] <- gsub(";", "|", data[, 231])
 rownames(data) <- data[, 231]
 
