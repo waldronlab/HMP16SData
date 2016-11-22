61821d9aa25aa093050e5a702495d85d45efb662
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-11-22 16:20:52 -0500

    Update Untitled.R

--- R/Untitled.R
+++ R/Untitled.R
@@ -1,10 +1,36 @@
+library(readr)
+library(magrittr)
+library(dplyr)
+
 # read the tsv file
-data <- read_tsv("./myfile.tsv")
+v13_map <- read_tsv("./data/v13_map_uniquebyPSN.txt")
+v13_otu <- read_tsv("./data/otu_table_psn_v13.txt", skip = 1)
+v35_map <- read_tsv("./data/v35_map_uniquebyPSN.txt")
+v35_otu <- read_tsv("./data/otu_table_psn_v35.txt", skip = 1)
+
+# make new dataframes by bodysite
+v13_Stool_map <- v13_map %>% filter(HMPbodysubsite == "Stool")
+v13_Saliva
+v13_Tongue_dorsum
+v13_Hard_palate
+v13_Buccal_mucosa
+v13_Attached_Keratinized_gingiva
+v13_Palatine_Tonsils
+v13_Throat
+v13_Supragingival_plaque
+v13_Subgingival_plaque
+v13_Anterior_nares
+v13_Left_Retroauricular_crease
+v13_Right_Retroauricular_crease
+v13_Right_Antecubital_fossa
+v13_Vaginal_introitus
+v13_Mid_vagina
+v13_Posterior_fornix
+v13_Left_Antecubital_fossa
 
 # add the rownames from last column
-data[, 233] <- gsub("Root;", "", data[, 233])
-data[, 231] <- gsub(";", "|", data[, 231])
-rownames(data) <- data[, 231]
+v13_Stool_otu <- v13_otu[, colnames(v13_otu) %in% v13_Stool$`#SampleID`]
+
 
 # map the colnames to bodysite and sample ID
 
