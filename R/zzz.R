0414ca34d9b308f28def01dfdf870cfd7cbe7a6b
schifferl <schifferl@users.noreply.github.com>
2016-12-29 10:09:57 -0500

    updated CI .yml files

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
-.onLoad <- function(libname, pkgname) {
-    # fl <- system.file("extdata", "metadata.csv", package = "HMP16SData")
-    # titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
-    # createHubAccessors(pkgname, titles)
-}
+# .onLoad <- function(libname, pkgname) {
+#     fl <- system.file("extdata", "metadata.csv", package = "HMP16SData")
+#     titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+#     createHubAccessors(pkgname, titles)
+# }
