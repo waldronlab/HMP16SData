b6b2944b7c1cd7e6ef31d2b1a028a32c2d39bda3
schifferl <schifferl@users.noreply.github.com>
2016-12-29 11:02:18 -0500

    missing comma in description file, RFC 4180 compliant csv

--- inst/scripts/downloadFiles.R
+++ inst/scripts/downloadFiles.R
@@ -1,12 +1,4 @@
 downloadFiles <- function() {
     system.file("extdata", "data.csv", package = "HMP16SData") %>%
-    read_csv(col_types = cols_only(
-        `File Description` = col_character(),
-        `v13 Download` = col_character(),
-        `v13 Size` = col_integer(),
-        `v13 MD5` = col_character(),
-        `v35 Download` = col_character(),
-        `v35 Size` = col_integer(),
-        `v35 MD5` = col_character()
-    ))
+    read_csv()
 }
