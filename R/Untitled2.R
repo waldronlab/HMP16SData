1087700ea9ff9e5a50e8508d0f423f97ad6e6276
schifferl <schifferl@users.noreply.github.com>
2017-02-01 17:06:43 -0500

    work done with Rimsha to explore the 16S data

--- R/Untitled2.R
+++ R/Untitled2.R
@@ -1,5 +1,7 @@
 library(readr)
 library(dplyr)
+library(Biobase)
+
 
 v13_otu <- read_tsv("http://downloads.hmpdacc.org/data/HMQCP/otu_table_psn_v13.txt.gz", skip = 1)
 v13_otu <- v13_otu[,c(1, 2912, 2:2911)]
@@ -40,3 +42,62 @@ ppAll_V13_map <- read_tsv(url_ppAll_V13_map, col_names = correct_cols, skip = 1)
 ppAll_V35_map <- read_tsv(url_ppAll_V35_map, col_names = correct_cols, skip = 1)
 
 # add SRS identifiers as colnames
+
+rownames(v13_otu) <- v13_otu$OTU.ID
+
+v13_otu$Consensus.Lineage %<>%
+    gsub("Root;", "", .) %>%
+    gsub(";", "|", .)
+
+v13_otu <- v13_otu[, -1]
+
+colnames(v13_otu) %>%
+    match(ppAll_V13_map$PSN) %>% {
+        sum(is.na(.))
+    }
+v13_Left_Antecubital_fossa_map <- ppAll_V13_map %>% filter(HMPBodySubsite == "Left_Antecubital_fossa")
+
+wanted_columns <- v13_Left_Antecubital_fossa_map$PSN
+wanted_columns <- wanted_columns[1:211]
+wanted_columns <- as.character(wanted_columns)
+
+v13_otu %>%
+    select(one_of(wanted_columns)) ->
+v13_Left_Antecubital_fossa_otu
+
+v13_Left_Antecubital_fossa_map <- as.data.frame(v13_Left_Antecubital_fossa_map)
+rownames(v13_Left_Antecubital_fossa_map) <- v13_Left_Antecubital_fossa_map$PSN
+
+AnnotatedDataFrame(v13_Left_Antecubital_fossa_map)
+
+v13_Left_Antecubital_fossa_map %<>%
+    select(PSN, NAP, SRS_SampleID, Region, BarcodeSequence,
+           LinkerPrimerSequence, Sex, HMPBodySubsite, HMPBodySite, VisitNo)
+
+v13_Left_Antecubital_fossa_map %>%
+    distinct() %>% filter(PSN == colnames(v13_Left_Antecubital_fossa_otu))
+
+
+pheno_data <- v13_Left_Antecubital_fossa_map[1:144, ]
+rownames(pheno_data) <- pheno_data$PSN
+AnnotatedDataFrame(pheno_data)
+ExpressionSet(data.matrix(v13_Left_Antecubital_fossa_otu))
+
+
+subset_colnames <- colnames(v13_Left_Antecubital_fossa_otu)
+subset_matches <- match(subset_colnames, v13_Left_Antecubital_fossa_map$PSN)
+
+v13_Left_Antecubital_fossa_map <- v13_Left_Antecubital_fossa_map[subset_matches, ]
+
+v13_Left_Antecubital_fossa_otu %>%
+    match(v13_Left_Antecubital_fossa_map$PSN, colnames(v13_Left_Antecubital_fossa_otu))
+
+match(colnames(v13_Left_Antecubital_fossa_otu), v13_Left_Antecubital_fossa_map$PSN)
+
+
+setOtherRownames <- function(x, y) {
+    print(x)
+    print(y)
+}
+
+setOtherRownames(9, 5)
