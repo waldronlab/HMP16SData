be1ee582715057a86076419cc128e4d8ee2ff833
Lucas Schiffer <schifferl@users.noreply.github.com>
2017-10-06 20:23:22 +0000

    additional changes

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+#' @importFrom utils read.csv
+#' @importFrom ExperimentHub createHubAccessors
+#'
+#' @keywords internal
+.onLoad <- function(libname, pkgname) {
+    fl <- system.file("extdata", "metadata.csv", package=pkgname)
+    titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
+    createHubAccessors(pkgname, titles)
+}
