44ac3a0daf64ed39915c19e2311d3f64db61ac68
schifferl <schifferl@users.noreply.github.com>
2018-02-20 17:33:02 -0500

    addition of attach_dbGaP function

---
+++ R/attach_dbGaP.R
@@ -0,0 +1,17 @@
+attach_dbGaP <- function(dbGaP_repository_key) {
+
+}
+
+# a <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht001184.v3.p1.EMMES_HMP_Subject.MULTI.txt.gz", skip = 10)
+#
+# b <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht001185.v4.p1.EMMES_HMP_Sample.MULTI.txt.gz", skip = 10)
+#
+# c <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht001187.v3.p1.c1.EMMES_HMP_DCM.HMP.txt.gz", skip = 10)
+#
+# d <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht001193.v3.p1.c1.EMMES_HMP_GTV.HMP.txt.gz", skip = 10)
+#
+# e <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht002156.v1.p1.c1.EMMES_HMP_DSU.HMP.txt.gz", skip = 10)
+#
+# f <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht002157.v1.p1.c1.EMMES_HMP_DTP_DHX_DVD.HMP.txt.gz", skip = 10)
+#
+# g <- read_tsv("~/ncbi/dbGaP-12146/files/phs000228.v4.pht002158.v1.p1.c1.EMMES_HMP_DEM_ENR.HMP.txt.gz", skip = 10)
