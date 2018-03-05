a5090fd76a10845d01ea8b694b7b3536299fda3c
schifferl <schifferl@users.noreply.github.com>
2018-03-05 13:59:04 +0000

    addition of data dictionary

---
+++ R/dictionary.R
@@ -0,0 +1,24 @@
+#' Data dictionary for ...
+#'
+#' A dictionary containing ...
+#'
+#' @format A data frame with 787 rows and 8 variables:
+#' \describe{
+#'   \item{Variable}{...}
+#'   \item{Description}{...}
+#'   \item{Type}{...}
+#'   \item{Units}{...}
+#'   \item{Minimum}{...}
+#'   \item{Maximum}{...}
+#'   \item{Coded values}{...}
+#'   \item{Comment}{...}
+#' }
+#' @source
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht001184.v3.EMMES_HMP_Subject.data_dict.xml}
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht001185.v4.EMMES_HMP_Sample.data_dict.xml}
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht001187.v3.EMMES_HMP_DCM.data_dict.xml}
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht001193.v3.EMMES_HMP_GTV.data_dict.xml}
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht002156.v1.EMMES_HMP_DSU.data_dict.xml}
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht002157.v1.EMMES_HMP_DTP_DHX_DVD.data_dict.xml}
+#' \url{ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/phs000228/phs000228.v4.p1/pheno_variable_summaries/phs000228.v4.pht002158.v1.EMMES_HMP_DEM_ENR.data_dict.xml}
+"dictionary"
