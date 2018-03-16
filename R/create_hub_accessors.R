4a286f390beb910d6ba6ea27dc58325b429e164d
schifferl <schifferl@users.noreply.github.com>
2018-03-16 19:31:15 +0000

    updated on load functionality

---
+++ R/create_hub_accessors.R
@@ -0,0 +1,42 @@
+#' @keywords internal
+#'
+#' @importFrom magrittr %>%
+#' @importFrom utils read.csv
+#' @importFrom magrittr use_series
+#' @importFrom ExperimentHub ExperimentHub
+#' @importFrom AnnotationHub query
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr extract
+create_hub_accessors <- function(pkgname) {
+    titles <-
+        system.file("extdata", "metadata.csv", package = pkgname) %>%
+        read.csv(stringsAsFactors = FALSE) %>%
+        use_series("Title") %>%
+        unique()
+
+    eh <-
+        ExperimentHub() %>%
+        query(pkgname)
+
+    ns <- asNamespace(pkgname)
+
+    for (title in titles) {
+        ehid <-
+            query(eh, title) %>%
+            names()
+
+        if (length(ehid) == 0L) {
+            sQuote(title) %>%
+                stop(" not found in ExperimentHub")
+        } else if (length(ehid) != 1L) {
+            ehid %<>%
+                rev() %>%
+                extract(1)
+        }
+
+        accessor <- ExperimentHub:::.hubAccessorFactory(ehid)
+
+        assign(title, accessor, envir = ns)
+        namespaceExport(ns, title)
+    }
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,8 @@
 #' @keywords internal
 #'
-#' @importFrom magrittr %>%
-#' @importFrom utils read.csv
-#' @importFrom magrittr use_series
-#' @importFrom ExperimentHub createHubAccessors
 #' @importFrom utils globalVariables
 .onLoad <- function(libname, pkgname) {
-    system.file("extdata", "metadata.csv", package = pkgname) %>%
-        read.csv(stringsAsFactors = FALSE) %>%
-        use_series("Title") %>%
-        unique() %>%
-        createHubAccessors(pkgname = pkgname, titles = .)
+    create_hub_accessors(pkgname)
 
     globalVariables(".")
     globalVariables("paths")
