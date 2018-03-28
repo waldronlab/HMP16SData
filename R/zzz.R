c0d30f4a29ec63cfc0ec068587b1417bdf755966
schifferl <schifferl@users.noreply.github.com>
2018-03-28 23:53:12 +0000

    add a global variable to suppress a NOTE in R CMD Check

--- R/zzz.R
+++ R/zzz.R
@@ -4,11 +4,12 @@
 .onLoad <- function(libname, pkgname) {
     create_hub_accessors(pkgname)
 
+    globalVariables("CONSENSUS_LINEAGE")
     globalVariables(".")
+    globalVariables("phylogeneticTree")
     globalVariables("paths")
     globalVariables("BioSample Accession")
     globalVariables("SN")
-    globalVariables("CONSENSUS_LINEAGE")
     globalVariables("Freq")
     globalVariables("N")
     globalVariables("%")
