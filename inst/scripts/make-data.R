626e49d378e1b81f605bb4cc94816f8e8800f5b8
schifferl <schifferl@users.noreply.github.com>
2018-03-29 18:54:22 +0000

    rewrite make-data.R documentation to reflect programatic downloading of files from HMP DACC

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,12 +1,14 @@
 # I. Downloading Data
 # ==============================================================================
-# Therein lies the problem, the data from the Human Microbiome (HMP) Website,
-# specifically the mapping files for variable regions 1–3 and 3–5, as relates to
-# data processed through the QIIME pipeline are no longer available. And when
-# they were available, they were not downloaded programatically — could have,
-# should have, would have. Anyway, the files included in the inst/extdata
-# directory are exactly as downloaded from the HMP website but there is no MD5
-# to check against. How about an ASCII high five instead?
+# Data from the Human Microbiome (HMP) Data Analysis and Coordination Center
+# (DACC) Website was downloaded programatically on March 28, 2018, as outlined
+# in data-raw/download.R script. An additional file, inst/extdata/data.csv, was
+# downloaded from the HMP DACC via a web browser such that MD5 sums of files
+# downloaded programatically could be checked against it. Two files downloaded
+# by the data-raw/download.R script do not have MD5 sums present in the
+# inst/extdata/data.csv file and, for provenance, the MD5 sums of these files
+# were written to a text file, inst/extdata/MD5.txt – this step occurs at the
+# end of the data-raw/download.R script. Up top for reproducibility!
 #
 #  ////
 # !!!!  _     \\\\
@@ -17,24 +19,35 @@
 #   /    |     \   (
 #   \
 #
-# Basically, users will just have to have faith that the raw files are
-# unadulturated, which they are, and that perhaps sometime in the future the HMP
-# Data Analysis and Coordination Center (DACC) will fix the issue. What is the
-# issue? There are two URLs
-# (http://downloads.ihmpdcc.org/data/HMQCP/v13_map_uniquebyPSN.txt.bz2 and
-# http://downloads.ihmpdcc.org/data/HMQCP/v35_map_uniquebyPSN.txt.bz2) that are
-# not readable - well they were once upon a time until someone updated the files
-# with a strange encoding. So, the point, the files in inst/extdata are these
-# files but the same results could not be achieved with the present URLs. Ok, so
-# data were download some time ago and put into inst/extdata - specifically
-# mapping and otu table files. The URLs of the mapping files are above and the
-# otu table were downloaded from
-# (http://downloads.ihmpdcc.org/data/HMQCP/otu_table_psn_v13.txt.gz and
-# http://downloads.ihmpdcc.org/data/HMQCP/otu_table_psn_v35.txt.gz). All this,
-# just to say that for complete reproducibility it would be necessary to use the
-# files exactly as they are in inst/extdata, if a reproduction of this work was
-# attempted. Basically, files were downloaded some time ago and then placed in
-# the inst/extdata directory.
+# What was downloaded? Excluding the inst/extdata/data.csv file, eight files in
+# total were downloaded from the HMP DACC – four files for variable regions 1–3
+# and four files for variable regions 3–5. The files corresponds to HMP 16S rRNA
+# sequencing data processed through the QIIME pipeline, this is outlined in the
+# R documentation, that were used to make SummarizedExperiment objects – the
+# following files were integrated into the SummarizedExperiment objects.
+#
+# OTU table files with PSN identifiers:
+# http://downloads.ihmpdcc.org/data/HMQCP/otu_table_psn_v13.txt.gz
+# http://downloads.ihmpdcc.org/data/HMQCP/otu_table_psn_v35.txt.gz
+#
+# Subject metadata files with PSN identifiers:
+# http://downloads.ihmpdcc.org/data/HMQCP/v13_map_uniquebyPSN.txt.bz2
+# http://downloads.ihmpdcc.org/data/HMQCP/v35_map_uniquebyPSN.txt.bz2
+#
+# Subject metadata files with SRS identifiers:
+# https://www.hmpdacc.org/hmp/doc/ppAll_V13_map.txt
+# https://www.hmpdacc.org/hmp/doc/ppAll_V35_map.txt
+#
+# Representative sequence phylogenetic trees:
+# http://downloads.ihmpdcc.org/data/HMQCP/rep_set_v13.tre.gz
+# http://downloads.ihmpdcc.org/data/HMQCP/rep_set_v35.tre.gz
+#
+# The unadulturated raw files are all available in inst/extdata and were
+# downloaded with the following command.
+
+source("data-raw/download.R")
+
+# Feel free to check the files against the MD5 sums if you like!
 #
 # II. Processing Data
 # ==============================================================================
@@ -43,7 +56,6 @@
 # seen in the data-raw directory. With the package as the working directory, the
 # data processing could be repeated with these commands:
 
-source("data-raw/download.R")
 source("data-raw/V13.R")
 source("data-raw/V35.R")
 
