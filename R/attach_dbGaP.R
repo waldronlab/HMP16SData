1b8589c529b07b03e4f530dbe645ed80fa40d106
schifferl <schifferl@users.noreply.github.com>
2020-07-08 10:41:52 +0000

    fix dbGaP functionality

--- R/attach_dbGaP.R
+++ R/attach_dbGaP.R
@@ -24,16 +24,7 @@
 #' The NCBI SRA Toolkit is called internally and must be on the user's
 #' \code{PATH} or an error message will be displayed. The NCBI SRA Toolkit can
 #' readily be installed on multiple platforms following
-#' \href{https://tinyurl.com/y845ppaa}{these} instructions or by one of the
-#' following methods.
-#' \subsection{Debian/Ubuntu}{
-#'     Using the \href{https://wiki.debian.org/Apt}{apt} package manager:\cr\cr
-#'     \code{apt install sra-toolkit}
-#' }
-#' \subsection{macOS}{
-#'     Using the \href{https://brew.sh/}{homebrew} package manager:\cr\cr
-#'     \code{brew install sratoolkit}
-#' }
+#' \href{https://tinyurl.com/y894uvvf}{these} instructions.
 #'
 #' @seealso \code{\link{dictionary}}
 #'
@@ -53,22 +44,17 @@ attach_dbGaP <- function(x, dbGaP_repository_key = "") {
     Sys.which("vdb-config") %>%
         nchar() %>%
         if (. == 0) {
-            stop("Ensure NCBI SRA Toolkit is installed and on your PATH. \n\n",
-                 "Debian/Ubuntu: \n\t",
-                 "apt install sra-toolkit \n",
-                 "macOS: \n\t",
-                 "brew install sratoolkit \n",
-                 "Windows: \n\t",
-                 "see https://tinyurl.com/y845ppaa \n",
+            stop("Ensure NCBI SRA Toolkit is installed and on your PATH.\n",
+                 "       See https://tinyurl.com/y894uvvf for help.\n",
+                 "       Check with Sys.getenv(\"PATH\")",
                  call. = FALSE)
         }
 
-    if (!dir.exists(paths$files_directory_path)) {
+    if (!dir.exists(paths$dbGaP_directory)) {
         assert_that(is.readable(dbGaP_repository_key))
         assert_that(has_extension(dbGaP_repository_key, "ngc"))
 
-        getwd() %>%
-            download_dbGaP(dbGaP_repository_key)
+        download_dbGaP(dbGaP_repository_key)
     }
 
     message("\nJoining the colData...\n")
