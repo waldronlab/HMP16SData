9ee7ebd07fa8193daadc2fd537bfa0221353c458
schifferl <schifferl@users.noreply.github.com>
2020-08-11 16:12:29 +0000

    fix dbGaP functionality, again

--- R/read_dbGaP.R
+++ R/read_dbGaP.R
@@ -2,14 +2,17 @@
 #'
 #' @importFrom magrittr %>%
 #' @importFrom dplyr rename
-read_dbGaP <- function() {
-    list(paths$EMMES_HMP_Sample_file_path,
-         paths$EMMES_HMP_GTV_file_path,
-         paths$EMMES_HMP_DTP_DHX_DVD_file_path,
-         paths$EMMES_HMP_DCM_file_path,
-         paths$EMMES_HMP_Subject_file_path,
-         paths$EMMES_HMP_DEM_ENR_file_path,
-         paths$EMMES_HMP_DSU_file_path) %>%
+read_dbGaP <- function(dbGaP_directory) {
+    list(paths$EMMES_HMP_Sample_file,
+         paths$EMMES_HMP_GTV_file,
+         paths$EMMES_HMP_DTP_DHX_DVD_file,
+         paths$EMMES_HMP_DCM_file,
+         paths$EMMES_HMP_Subject_file,
+         paths$EMMES_HMP_DEM_ENR_file,
+         paths$EMMES_HMP_DSU_file) %>%
+        lapply(c, dbGaP_directory) %>%
+        lapply(rev) %>%
+        lapply(paste, collapse = .Platform$file.sep) %>%
         lapply(quiet_read) %>%
         Reduce(quiet_join, x = .) %>%
         rename(BioSample_Accession = `BioSample Accession`)
