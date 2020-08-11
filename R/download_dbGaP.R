bd3d50c980f7f55703075df3926633243c0c8bc6
schifferl <schifferl@users.noreply.github.com>
2020-08-11 16:12:29 +0000

    fix dbGaP functionality, again

--- R/download_dbGaP.R
+++ R/download_dbGaP.R
@@ -1,12 +1,12 @@
 #' @keywords internal
 #'
 #' @importFrom magrittr %>%
-download_dbGaP <- function(dbGaP_repository_key) {
+download_dbGaP <- function(dbGaP_repository_key, dbGaP_directory) {
     system.file("extdata/cart_DAR48406_201802201501.krt",
                 package = "HMP16SData") %>%
         paste("prefetch", ., "--ngc", dbGaP_repository_key) %>%
         system()
 
-    paste("vdb-decrypt", paths$dbGaP_directory, "--ngc", dbGaP_repository_key) %>%
+    paste("vdb-decrypt", dbGaP_directory, "--ngc", dbGaP_repository_key) %>%
         system()
 }
